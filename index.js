b7376a132d41ccfb230efc46ee8899b87d11d131
DexterDC <sebasdc9@hotmail.com>
2024-10-21 11:58:48 -0600

    add json express

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import departmentRoutes from "./src/routes/deparments/department.js";
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
