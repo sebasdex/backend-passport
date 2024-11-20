a53b8b8d24303995ac07d3d62729a79cea8d4dfe
DexterDC <sebasdc9@hotmail.com>
2024-11-20 14:12:48 -0600

    add secure cookie

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24,
         httpOnly: true,
         sameSite: "lax",
