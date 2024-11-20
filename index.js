66ce3b3b2ac7bb1a824fc0a534930f8559f8a502
DexterDC <sebasdc9@hotmail.com>
2024-11-20 14:22:21 -0600

    update session

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true,
+        secure: false,
         maxAge: 1000 * 60 * 60 * 24,
         httpOnly: true,
         sameSite: "lax",
